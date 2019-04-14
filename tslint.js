841d6b1ee5f055fba2dbfd1134791dc4871b1826
Gerkin <nihilivin@gmail.com>
2019-04-14 08:54:15 +0200

    feat: Lint ts & js files with specific rules

--- tslint.js
+++ tslint.js
@@ -1,72 +1,78 @@
-const {join, dirname} = require('path');
+const { join, dirname } = require( 'path' );
+
+const commonRules = {
+	'align': {
+		'options': ['arguments', 'elements', 'members', 'parameters', 'statements'],
+	},
+	'arrow-parens': {
+		'options': ['ban-single-arg-parens'],
+	},
+	'arrow-return-shorthand': {
+		'options': ['multiline'],
+	},
+	'completed-docs': {
+		'options': ['enums', 'functions', 'methods'],
+	},
+	'indent': {
+		'options': ['tabs', 4],
+	},
+	'jsdoc-format': {
+		'options': ['check-multiline-start'],
+	},
+	'linebreak-style': {
+		'options': ['LF'],
+	},
+	'max-line-length': {
+		'options': [200],
+	},
+	'member-ordering': false,
+	'no-duplicate-variable': {
+		'options': ['check-parameters'],
+	},
+	'no-implicit-dependencies': true,
+	'no-invalid-template-strings': true,
+	'no-irregular-whitespace': true,
+	'no-namespace': false,
+	'no-return-await': true,
+	'object-curly-spacing': [true, 'always', { 'objectsInObjects': false }],
+	'prefer-template': {
+		'options': ['allow-single-concat'],
+	},
+	'quotemark': {
+		'options': ['single', 'avoid-escape', 'avoid-template'],
+	},
+	'space-within-parens': {
+		'options': [1],
+	},
+	'variable-name': {
+		'options': ['ban-keywords', 'check-format', 'allow-pascal-case', 'allow-leading-underscore'],
+	},
+};
 
 module.exports = {
-    "extends": "tslint:recommended",
-    "defaultSeverity": "error",
-    "rulesDirectory": [
-        join(dirname(require.resolve("tslint-eslint-rules")), "dist/rules"),
-    ],
-    "rules": {
-        "align": {
-            "options": ["arguments", "elements", "members", "parameters", "statements"]
-        },
-        "array-type": {
-            "options": "array-simple"
-        },
-        "arrow-parens": {
-            "options": ["ban-single-arg-parens"]
-        },
-        "arrow-return-shorthand": {
-            "options": ["multiline"]
-        },
-        "await-promise": true,
-        "completed-docs": {
-            "options": ["enums", "functions", "methods"]
-        },
-        "indent": {
-            "options": ["tabs", 4]
-        },
-        "jsdoc-format": {
-            "options": ["check-multiline-start"]
-        },
-        "linebreak-style": {
-            "options": ["LF"]
-        },
-        "max-line-length": {
-            "options": [200]
-        },
-        "member-access": {
-            "options": ["check-accessor", "check-constructor", "check-parameter-property"]
-        },
-        "member-ordering": false,
-        "no-boolean-literal-compare": true,
-        "no-duplicate-variable": {
-            "options": ["check-parameters"]
-        },
-        "no-floating-promises": true,
-        "no-implicit-dependencies": true,
-        "no-inferred-empty-object-type": true,
-        "no-invalid-template-strings": true,
-        "no-irregular-whitespace": true,
-        "no-namespace": false,
-        "no-redundant-jsdoc": true,
-        "no-return-await": true,
-        "no-unnecessary-qualifier": true,
-        "no-unused-variable": true,
-        "object-curly-spacing": [true, "always", { "objectsInObjects": false }],
-        "prefer-readonly": true,
-        "prefer-template": {
-            "options": ["allow-single-concat"]
-        },
-        "quotemark": {
-            "options": ["single", "avoid-escape", "avoid-template"]
-        },
-        "space-within-parens": {
-            "options": [1]
-        },
-        "type-literal-delimiter": true,
-        "variable-name": {
-            "options": ["ban-keywords", "check-format", "allow-pascal-case", "allow-leading-underscore"]
-        }
-    }
-}
+	'defaultSeverity': 'error',
+	'extends': 'tslint:recommended',
+	'rulesDirectory': [
+		join( dirname( require.resolve( 'tslint-eslint-rules' ) ), 'dist/rules' ),
+	],
+	'rules': {
+		...commonRules,
+		'array-type': {
+			'options': 'array-simple',
+		},
+		'await-promise': true,
+		'member-access': {
+			'options': ['check-accessor', 'check-constructor', 'check-parameter-property'],
+		},
+		'no-boolean-literal-compare': true,
+		'no-floating-promises': true,
+		'no-inferred-empty-object-type': true,
+		'no-redundant-jsdoc': true,
+		'no-unnecessary-qualifier': true,
+		'prefer-readonly': true,
+		'type-literal-delimiter': true,
+	},
+	'jsRules': {
+		...commonRules,
+	},
+};
